e25a51cc17034f0a9533fede5472b8b174bd1ba2
pablo609 <bmw602@o2.pl>
2016-06-05 18:09:33 +0200

    v1

--- README.md
+++
@@ -1,2 +0,0 @@
-# java
-This repository is only for private learning purpose.
---
+++ test1.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+class test{
+	public static void main(String[] args){
+		for(int xCount = 0; xCount< 10; xCount++){
+			System.out.println(Math.random());
+		}
+	}
+}
