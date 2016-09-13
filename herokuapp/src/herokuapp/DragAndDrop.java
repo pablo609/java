687505e70c6daa3a8ea3c8b41286c75b1929c7f3
pablo609 <bmw602@o2.pl>
2016-09-13 22:35:14 +0200

    Modified DragAndDrop.java

--- herokuapp/src/herokuapp/DragAndDrop.java
+++ herokuapp/src/herokuapp/DragAndDrop.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -18,28 +20,34 @@ public class DragAndDrop {
 	public void setUp() {
 		driver = new FirefoxDriver();
 		driver.get("http://the-internet.herokuapp.com");
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.findElement(By.cssSelector("a[href='/drag_and_drop'")).click();
 	}
 	
+	@AfterClass
 	public void tearDown() {
-		
+		driver.close();
 	}
 	
-	@Test
+	@Test(enabled = false)
 	public void test() throws Exception {
-		WebElement elementA = driver.findElement(By.cssSelector("div#column-a>header"));
-		WebElement elementB = driver.findElement(By.cssSelector("div#column-b>header"));
-		
-		Assert.assertEquals(elementA.getText(), "A");
-		Assert.assertEquals(elementB.getText(), "B");
-		
-		Actions builder = new Actions(driver);
-		//builder.moveToElement(elementA, 20, 100).clickAndHold().moveToElement(elementB, 20, 100).release().perform();
-		builder.moveToElement(elementA).contextClick().perform();
-		Thread.sleep(3000);
-		//builder.dragAndDropBy(elementA, elementB.getLocation().getX()+10, elementB.getLocation().getY()+10).perform();
-		
+		if(true) {
+			throw new SkipException("Drag And Drop doesn't work in HTML5");
+		}
+		else {
+			WebElement elementA = driver.findElement(By.cssSelector("div#column-a"));
+			WebElement elementB = driver.findElement(By.cssSelector("div#column-b"));
+			
+			Assert.assertEquals(elementA.getText(), "A");
+			Assert.assertEquals(elementB.getText(), "B");
+			
+			Actions builder = new Actions(driver);
+	
+			builder.dragAndDrop(elementA, elementB).perform();
+	
+			Assert.assertEquals(elementA.getText(), "B");
+			Assert.assertEquals(elementB.getText(), "A");
+		}
 	}
 }
