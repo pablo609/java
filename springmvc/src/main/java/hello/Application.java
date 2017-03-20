d7b227ca8b844f925bac2e880dede3aee13fc828
pablo609 <bmw602@o2.pl>
2017-03-20 21:52:12 +0100

    spring

---
+++ springmvc/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pawel on 2017-02-23.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ springmvc/src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+
+/**
+ * Created by pawel on 2017-02-23.
+ */
+@Controller
+public class GreetingController {
+    @RequestMapping(method=GET, path="/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
